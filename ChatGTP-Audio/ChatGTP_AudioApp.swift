e73e8ada9d995af760ba6f794b73081e76f91dde
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2023-06-03 15:23:02 +0200

    Initial Commit

---
+++ ChatGTP-Audio/ChatGTP_AudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGTP_AudioApp.swift
+//  ChatGTP-Audio
+//
+//  Created by Tommy Ludwig on 03.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGTP_AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
